e783dd1ad06dcfaf8c76618e845a4854dfa84372
yusufr98 <yusuf090798@gmail.com>
2019-10-07 13:46:47 -0400

    initial commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Student {
+    private String name;
+    private int age;
+    private String height;
+    private String PhoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+}
